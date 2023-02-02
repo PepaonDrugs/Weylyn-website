0088736c59d09e52cdde12b7e890bb726a064bfa
Pepa on Drugs <93032314+PepaonDrugs@users.noreply.github.com>
2023-02-02 16:25:25 +0100

    Add files via upload

---
+++ stats/js/uptime.js
@@ -0,0 +1,12 @@
+function check_available(ip){
+  var el=document.getElementById("check_pic");
+  el.src="https://status.weylyn.net/upload/logo1.png?now="+Math.random();
+  var varible = 1;
+}
+
+
+function check_success(url){
+  serverstatus = "server is on";
+  document.getElementById("demo").innerHTML = "Server is online everything is Good";
+
+}
